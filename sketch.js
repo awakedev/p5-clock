801181f2b1582cbd3bf941b33b3752cc2f8f4dfe
Nathan Corry <32592701+Wander1ust@users.noreply.github.com>
2019-01-01 15:39:41 +0000

    styling

--- sketch.js
+++ sketch.js
@@ -1,13 +1,16 @@
 
 
 function setup() {
-        createCanvas(innerWidth, outerHeight);
+    var cnv = createCanvas(400 , 400);
+    var x = (windowWidth - width) / 2;
+    var y = (windowHeight - height) / 2;
+    cnv.position(x, y);
         angleMode(DEGREES);
       }
 
 
 function draw() {
-    background(0);
+    background(80);
     translate(200, 200);
     rotate(-90);
 
@@ -18,36 +21,37 @@ function draw() {
     strokeWeight(8);
     stroke(255,100,150);
     noFill();
+    
 
     let secAngle = map(sc, 0, 60, 0 , 360);
-    arc(0, 0, 300, 300, 0, secAngle);
+   // arc(0, 0, 300, 300, 0, secAngle);
 
     stroke(150,100,255);
     let minAngle = map(mn, 0, 60, 0 , 360);
-    arc(0, 0, 280, 280, 0, minAngle);
+   // arc(0, 0, 280, 280, 0, minAngle);
         
     stroke(150,255,100);
      let hrAngle = map(hr % 12, 0, 12, -90 , 360);
-     arc(0, 0, 260, 260, 0, hrAngle);
+    // arc(0, 0, 260, 260, 0, hrAngle);
 
      
     push();
     rotate(secAngle);
-    stroke(255);
+    stroke(255,100,150);
     line(0,0,100,0);
     pop();
 
 
     push();
     rotate(minAngle);
-    stroke(255);
+    stroke(150,100,255);
     line(0,0,75,0);
     pop();
 
     
     push();
     rotate(hrAngle);
-    stroke(255);
+    stroke(150,255,100);
     line(0,0,50,0);
     pop();
 
